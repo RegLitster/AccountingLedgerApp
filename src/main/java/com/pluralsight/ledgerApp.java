c89559c2c37245869c4d2185c4836a17207e60a1
Reggie <reglitster00@gmail.com>
2025-10-15 10:17:45 -0400

    Ledger display deposit and display payments work

--- src/main/java/com/pluralsight/ledgerApp.java
+++ src/main/java/com/pluralsight/ledgerApp.java
@@ -7,14 +7,15 @@ import java.util.Scanner;
 
 public class ledgerApp {
     public static void main(String[] args) {
+        Ledger ledger = new Ledger("","","","",0);
         Scanner input = new Scanner(System.in);
         System.out.println("=====Welcome To The Ledger App=====");
         while (true) {
-            displayLedger(input);
+            ledgerHome(input,ledger);
         }
 
     }
-    public static void displayLedger(Scanner input) {
+    public static void ledgerHome(Scanner input,Ledger ledger) {
         System.out.print("""
                 Please Enter A Selection Below
                 D: Add Deposit
@@ -25,49 +26,37 @@ public class ledgerApp {
         String selection = input.nextLine().trim().toUpperCase();
         switch (selection) {
             case "D":
-            Deposit(input);
-            break;
+                Deposit(input,ledger);
+                break;
             case "P":
-            Payment(input);
-            break;
+                Payment(input,ledger);
+                break;
             case "L":
-            Ledger(input);
-            break;
+                ledgerDisplay(input,ledger);
+                break;
             case "X":
-            input.close();
-            System.exit(0);
-            break;
+                input.close();
+                System.exit(0);
+                break;
         }
 
 
     }
-    public static void Deposit(Scanner input) {
+    public static void Deposit(Scanner input, Ledger ledger) {
         System.out.print("Enter Deposit Description: ");
         String description = input.nextLine();
 
         System.out.print("Enter Name From: ");
-        String name = input.nextLine();
+        String vendor = input.nextLine();
 
         System.out.print("Enter Deposit Amount: ");
         double amount = input.nextDouble();
         input.nextLine();
 
-        LocalDateTime now = LocalDateTime.now();
-        String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-
-        String transactionLine = String.format("%s|%s|%s|%s|%.2f", date, time, description, name, amount);
-
-        try (BufferedWriter bufWriter = new BufferedWriter(new FileWriter("src/main/resources/transactions.csv", true))) {
-            bufWriter.write(transactionLine);
-            bufWriter.newLine();
-            System.out.println("Deposited Successfully\n");
-        } catch (IOException e) {
-            System.out.println("An Unexpected Error Has Occurred " + e.getMessage());
-        }
+        ledger.saveTransaction(description,vendor,amount);
 
     }
-    public static void Payment(Scanner input) {
+    public static void Payment(Scanner input,Ledger ledger) {
         System.out.print("Enter Payment Description: ");
         String description = input.nextLine();
 
@@ -80,29 +69,83 @@ public class ledgerApp {
 
         amount = -Math.abs(amount);
 
-        double balance = 0.0;
+        ledger.saveTransaction(description,name,amount);
 
-        LocalDateTime now = LocalDateTime.now();
-        String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+    public static void ledgerDisplay(Scanner input,Ledger ledger) {
+        System.out.println("""
+                Please Make A Selection Below
+                A: Display All
+                D: Display Deposits
+                P: Display Payments
+                R: Reports
+                H: Return to Home
+                """);
+        String selection = input.nextLine().trim().toUpperCase();
+        switch (selection){
+            case "A":
+                displayAll();
+                break;
+            case "D":
+                displayDeposit();
+                break;
+            case "P":
+                displayPayments();
+                break;
+            case "R":
+                //     reports
+                break;
+            case "H":
+        }
 
-        String transactionLine = String.format("%s|%s|%s|%s|%.2f",date,time,description,name,amount);
+    }
+    public static void displayAll(){
+        try (BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/transactions.csv"))) {
+            String input;
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/transactions.csv", true))) {
-            writer.write(transactionLine);
-            writer.newLine();
-            System.out.println("Payment Was Successful.");
-            System.out.printf("-%.2f%n", balance + amount);
+            while((input = bufReader.readLine()) != null) {
+                System.out.println(input+"\n");
+            }
         } catch (IOException e) {
             System.out.println("An Unexpected Error Has Occurred " + e.getMessage());
         }
     }
-    public static void Ledger(Scanner input) {
 
+    public static void displayDeposit() {
+        try (BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/transactions.csv"))) {
+            String line;
+            bufReader.readLine();
 
+            while ((line = bufReader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                    double amount = Double.parseDouble(parts[4].trim());
+                    if (amount > 0) {
+                        System.out.println(line);
+                    }
+
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading deposits: " + e.getMessage());
+        }
+        System.out.println("");
     }
+    public static void displayPayments() {
+        try (BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/transactions.csv"))) {
+            String line;
+            bufReader.readLine();
 
-
+            while ((line = bufReader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                    double amount = Double.parseDouble(parts[4].trim());
+                    if (amount < 0) {
+                        System.out.println(line);
+                    }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading payments: " + e.getMessage());
+        }
+        System.out.println("");
+    }
 
 
 
